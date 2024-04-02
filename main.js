7a4bc9c9aa09c697c41c25231e48c42062e5fa7c
Marcusey <Marcus_y83@hotmail.com>
2024-04-02 10:36:20 +0200

    :art: improved js

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `you made number ${counter} be counted`
-  }
+  let counter = 0;
+
+  const updateCounterDisplay = () => {
+    element.innerHTML = `you made number ${counter} be counted`;
+  };
 
   const incrementCounter = () => {
-    counter++
-    updateCounterDisplay()
-  }
+    counter++;
+    updateCounterDisplay();
+  };
 
-  element.addEventListener('click', incrementCounter)
-  updateCounterDisplay()
+  element.addEventListener('click', incrementCounter);
+  updateCounterDisplay();
 }
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector('#counter'));
